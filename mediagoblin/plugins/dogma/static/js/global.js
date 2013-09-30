9520d0613e5c699c80d004cc51d1b55f093ff5f0
MusiqueLibre <dev@dogmazic.net>
2013-09-30 21:02:45 +0200

    markdown UI for the ajax

--- mediagoblin/plugins/dogma/static/js/global.js
+++ mediagoblin/plugins/dogma/static/js/global.js
@@ -145,15 +145,6 @@ $(function(){
   init()
   //"add member" setup as default
   addMember('_', true)
-  //add the markdown wysiwyg if there's the proper textarea input
-  if($('#wmd-input_0').length > 0){
-   $('#wmd-input_0').wrap('<div id="wmd-panel_0" class="visual_block"></div>')
-   $('#wmd-input_0').before('<div id="wmd-button-bar_0" class="wmd-button-bar"></div>')
-   $('#wmd-input_0').after('<h3>'+text_preview+'</h3></p><div id="wmd-preview_0"></div>')
-   converter = new Markdown.Converter();
-   editor = new Markdown.Editor(converter, '_0');
-   editor.run();
-  }
 });
 
 
@@ -581,4 +572,14 @@ function init(){
        itemSelector: '.dashboard_band'
   });
   var msnry = $container.data('masonry');
+  //TODO add some conditions before lunching stuffs
+  //add the markdown wysiwyg if there's the proper textarea input
+  if($('#wmd-input_0').length > 0){
+   $('#wmd-input_0').wrap('<div id="wmd-panel_0" class="visual_block"></div>')
+   $('#wmd-input_0').before('<div id="wmd-button-bar_0" class="wmd-button-bar"></div>')
+   $('#wmd-input_0').after('<h3>'+text_preview+'</h3></p><div id="wmd-preview_0"></div>')
+   converter = new Markdown.Converter();
+   editor = new Markdown.Editor(converter, '_0');
+   editor.run();
+  }
 }
